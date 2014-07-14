b283fd14a0083081fae87c89f07b0c09ed7b5f28
Dan Cogswell <cogswell@alum.mit.edu>
2014-07-14 15:39:59 -0400

    Adds standard functions for interfacial energy anisotropy.

--- phasefield3DMG.h
+++ phasefield3DMG.h
@@ -29,4 +29,32 @@ void d_plus_Nu_heat_eqn(grid3D&, grid3D&, grid3D&, double, double);
 void L_heat_eqn(grid3D&, int, int, double, double);
 void f_heat_eqn(grid3D&, grid3D&, double, double);
 
+//Anisotropy: 1+e4*cos(4*theta)
+#define e4 .025
+//-----------------------------------------------------------------------------
+inline double epsilon(grid3D& xi, int i, int j){
+  double XI=xi(i,j,0);
+  double e=1;
+  if (XI>.01 && XI<.99){
+    double dx=xi.DX(i,j,0,1);
+    double dy=xi.DY(i,j,0,1);
+    double cos2_t=sq(dx)/(sq(dx)+sq(dy));
+    double cos4t=8*sq(cos2_t)-8*cos2_t+1;
+    e=1+e4*cos4t;
+  }
+  return(e);
+}
+
+inline double depsilon_dtheta(grid3D& xi, int i, int j){
+  double XI=xi(i,j,0);
+  double de_dtheta=0;
+  if (XI>.01 && XI<.99){
+    double dx=xi.DX(i,j,0,1);
+    double dy=xi.DY(i,j,0,1);
+    double sin_4t=4*dx*dy*(sq(dx)-sq(dy))/(sq(dx)+sq(dy));
+    de_dtheta=-4*e4*sin_4t;
+  }
+  return(de_dtheta);
+}
+
 #endif
