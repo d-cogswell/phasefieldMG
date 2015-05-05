0b2e96079c27e655ebe8d08c86753681d2abdf38
Dan Cogswell <cogswell@alum.mit.edu>
2015-05-05 08:43:14 -0400

    Adds LHS functions to reduce code duplication.

--- phasefield3DMG.cpp
+++ phasefield3DMG.cpp
@@ -14,16 +14,20 @@ void GS_LEX_AC(grid3D& u, grid3D& f, double dt, double h){
   u.periodicBoundary();
 }
 //-----------------------------------------------------------------------------
+inline double AC_LHS(grid3D& u, double dt, double h, int i, int j, int k){
+  return(u(i,j,k)+dt*(2*u(i,j,k)-kappa*u.laplacian(i,j,k,h)));
+}
+//-----------------------------------------------------------------------------
 void dfct_AC(grid3D& d, grid3D& u, grid3D& f, double dt, double h){
   gridLoop3D(d){
-    d(i,j,k)=f(i,j,k)-(u(i,j,k)+dt*(2*u(i,j,k)-kappa*u.laplacian(i,j,k,h)));
+    d(i,j,k)=f(i,j,k)-AC_LHS(u,dt,h,i,j,k);
   }
 }
 //-----------------------------------------------------------------------------
 void d_plus_Nu_AC(grid3D& f, grid3D& d, grid3D& u,double dt, double h){
   u.periodicBoundary();
   gridLoop3D(f){
-    f(i,j,k)=d(i,j,k)+u(i,j,k)+dt*(2*u(i,j,k)-kappa*u.laplacian(i,j,k,h));
+    f(i,j,k)=d(i,j,k)+AC_LHS(u,dt,h,i,j,k);
   }
 }
 //-----------------------------------------------------------------------------
@@ -63,50 +67,45 @@ void GS_LEX_CH(grid3D& u, grid3D& f, double dt, double h){
     int j1=(j+1)%Ny, j2=(j+2)%Ny, j_1=(j+Ny-1)%Ny, j_2=(j+Ny-2)%Ny;
 
     u(i,j,k)=(-dt*(
-      kappa/(h*h*h*h)*(-8*u(i1,j,k)-8*u(i_1,j,k)-8*u(i,j1,k)-8*u(i,j_1,k)
-                   +2*u(i1,j1,k)+2*u(i_1,j_1,k)+2*u(i1,j_1,k)+2*u(i_1,j1,k)
-                   +u(i2,j,k)+u(i_2,j,k)+u(i,j2,k)+u(i,j_2,k))
+      kappa/(h*h*h*h)*(
+            -8*(u(i1,j,k)+u(i_1,j,k)+u(i,j1,k)+u(i,j_1,k))
+            +2*(u(i1,j1,k)+u(i_1,j_1,k)+u(i1,j_1,k)+u(i_1,j1,k))
+            +u(i2,j,k)+u(i_2,j,k)+u(i,j2,k)+u(i,j_2,k))
       -2/sq(h)*(u(i1,j,k)+u(i_1,j,k)+u(i,j1,k)+u(i,j_1,k))
       )+f(i,j,k))/D;
   }
   u.periodicBoundary();  
 }
 //-----------------------------------------------------------------------------
-void dfct_CH(grid3D& d, grid3D& u, grid3D& f,double dt, double h){
+inline double CH_LHS(grid3D& u, double dt, double h, int i, int j, int k){
   int Nx=u.getDimension(1);
   int Ny=u.getDimension(2);
-  gridLoop3D(d){
-    int i1=(i+1)%Nx, i2=(i+2)%Nx, i_1=(i+Nx-1)%Nx, i_2=(i+Nx-2)%Nx;
-    int j1=(j+1)%Ny, j2=(j+2)%Ny, j_1=(j+Ny-1)%Ny, j_2=(j+Ny-2)%Ny;
-    d(i,j,k)=f(i,j,k)-u(i,j,k)-dt*(
+  int i1=(i+1)%Nx, i2=(i+2)%Nx, i_1=(i+Nx-1)%Nx, i_2=(i+Nx-2)%Nx;
+  int j1=(j+1)%Ny, j2=(j+2)%Ny, j_1=(j+Ny-1)%Ny, j_2=(j+Ny-2)%Ny;
+  double LHS=u(i,j,k)+dt*(
       kappa/(h*h*h*h)*(20*u(i,j,k)
         -8*(u(i1,j,k)+u(i_1,j,k)+u(i,j1,k)+u(i,j_1,k))
         +2*(u(i1,j1,k)+u(i_1,j_1,k)+u(i1,j_1,k)+u(i_1,j1,k))
         +(u(i2,j,k)+u(i_2,j,k)+u(i,j2,k)+u(i,j_2,k)))
       -2*u.laplacian(i,j,k,h));
+  return(LHS);
+}
+//-----------------------------------------------------------------------------
+void dfct_CH(grid3D& d, grid3D& u, grid3D& f,double dt, double h){
+  gridLoop3D(d){
+    d(i,j,k)=f(i,j,k)-CH_LHS(u,dt,h,i,j,k);
   }
 }
 //-----------------------------------------------------------------------------
 void d_plus_Nu_CH(grid3D& f, grid3D& d, grid3D& u, double dt, double h){
-  int Nx=f.getDimension(1);
-  int Ny=f.getDimension(2);
   u.periodicBoundary();
   gridLoop3D(f){
-    int i1=(i+1)%Nx, i2=(i+2)%Nx, i_1=(i+Nx-1)%Nx, i_2=(i+Nx-2)%Nx;
-    int j1=(j+1)%Ny, j2=(j+2)%Ny, j_1=(j+Ny-1)%Ny, j_2=(j+Ny-2)%Ny;
-    f(i,j,k)=d(i,j,k)+u(i,j,k)+dt*(
-      kappa/(h*h*h*h)*(20*u(i,j,k)
-        -8*(u(i1,j,k)+u(i_1,j,k)+u(i,j1,k)+u(i,j_1,k))
-        +2*(u(i1,j1,k)+u(i_1,j_1,k)+u(i1,j_1,k)+u(i_1,j1,k))
-        +(u(i2,j,k)+u(i_2,j,k)+u(i,j2,k)+u(i,j_2,k)))
-      -2*u.laplacian(i,j,k,h));
+    f(i,j,k)=d(i,j,k)+CH_LHS(u,dt,h,i,j,k);
   }
 }
 //-----------------------------------------------------------------------------
 void f_CH(grid3D& f, grid3D& u, double dt, double h){
-  int Nx=u.getDimension(1);
-  int Ny=u.getDimension(2);
-  grid3D u_cubed(Nx,Ny,1);
+  grid3D u_cubed(u.N1,u.N2,u.N3);
   gridLoop3D(u_cubed){
     double u_val=u(i,j,k);
     u_cubed(i,j,k)=cube(u_val);
@@ -114,7 +113,7 @@ void f_CH(grid3D& f, grid3D& u, double dt, double h){
   u_cubed.periodicBoundary();
 
   gridLoop3D(f){
-    (f)(i,j,k)=dt*(u_cubed.laplacian(i,j,k,h)-3*u.laplacian(i,j,k,h))+u(i,j,k);
+    (f)(i,j,k)=u(i,j,k)+dt*(u_cubed.laplacian(i,j,k,h)-3*u.laplacian(i,j,k,h));
   }
 }
 //-----------------------------------------------------------------------------
